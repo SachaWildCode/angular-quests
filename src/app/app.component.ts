932f6c8fd3ebf45f8899b4f01dd90910c6f293bb
SachaWildCode <sacha.loumachi@gmail.com>
2024-05-06 16:07:54 +0200

    resolve quest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Bienvenue sur le site de Wilder';
+  title = 'Bonjour';
+  isAdmin:boolean = false;
+
+  changeStatus(){
+    this.isAdmin = !this.isAdmin;
+  }
 }
